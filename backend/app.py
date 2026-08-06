4d6e1476ff78521e566f24f72dcd9d89e4a60517
LichoGKZ <168931651+LichoGKZ@users.noreply.github.com>
2026-08-06 17:11:36 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -219,7 +219,8 @@ def send_purchase_event(row, value, currency, order_id, email=None):
                     "order_id": order_id,
                 },
             }
-        ]
+        ],
+        "test_event_code": "TEST73462"
     }
 
     url = f"https://graph.facebook.com/{FB_API_VERSION}/{FB_PIXEL_ID}/events"
